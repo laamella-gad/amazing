46aad2d592f06dde629cded21a9f36128930afe3
Danny van Bruggen <tsuihark@gmail.com>
2009-03-01 23:21:32 +0100

    Clean up of old crap.

--- amazing-core/src/main/java/com/laamella/amazing/generators/various/EllerMazeGeneratorC64.java
+++ amazing-core/src/main/java/com/laamella/amazing/generators/various/EllerMazeGeneratorC64.java
@@ -12,6 +12,54 @@ import com.laamella.amazing.mazemodel.orthogonal.Square;
 /**
  * Converted from a BASIC type in listing, found in a magazine somewhere in the
  * eighties.
+ * 
+ * <p>The original listing follows. There may be typos in the part starting at line 300.
+ * <ul>
+ * <li>CLS = reverse heart, clear screen
+ * <li>] = bar on the right
+ * <li>J = bar on the right and bottom
+ * <li>. = block lower right
+ * <li>_ = block on the bottom
+ * <li>{Q} = cursor down
+ * </ul>
+ * 
+ * <pre>
+ *  10 RUN80
+ *  20 P=A-1
+ *  30 IFR(P)THENP=R(P):GOTO30
+ *  40 L(R(A))=P:R(P)=R(A):RETURN
+ *  50 P=A
+ *  60 IFL(P)THENP=L(P):GOTO60
+ *  70 R(L(A-1))=P:L(P)=L(A-1):RETURN
+ *  80 POKE53280,0:POKE53281,0:POKE646,8:PRINT&quot;CLS&quot;
+ *  140 C$(0)=&quot;]&quot;:C$(1)=&quot;J&quot;:C$(2)=&quot;.&quot;:C$(3)=&quot;_&quot;:POKE53281,0
+ *  150 PRINT:INPUT&quot;{Q}BREEDTE VAN DOOLHOF  (1-39)&quot;;X::IFX&lt;1ORX&gt;39THEN150
+ *  160 PRINT:INPUT&quot;{Q}HOOGTE VAN DOOLHOF   (2-X)&quot;;H:IFH&lt;2THEN150
+ *  170 INPUT&quot;STEILHEID ( 0 - .5 - 1 )&quot;;S:IFS&lt;0ORS&gt;1THEN170
+ *  180 Q$=&quot;&quot;:IFH&lt;24THENINPUT&quot;OPLOSSEN?(J/N)&quot;;Q$:IFQ$&lt;&gt;&quot;J&quot;ANDQ$&lt;&gt;&quot;N&quot;THEN180
+ *  190 PRINT&quot;CLS.&quot;;:DIML(X),R(X):I=INT(RND(1)*X)+1
+ *  200 FORA=1TOX:PRINTC$(3+(A=I));:L(A)=0:R(A)=0:NEXT:PRINTSPC(39-X);&quot;]&quot;;
+ *  210 FORY=2TOH:FORA=XTO1STEP-1:IFR(A)=A-1ORRND(1)&lt;STHENZ=0:GOTO250
+ *  220 IFR(A)THENGOSUB20
+ *  230 IFL(A-1)THENGOSUB50
+ *  240 R(A)=A-1:L(A-1)=A:Z=2
+ *  250 IFL(A)+R(A) AND RND(1)&gt;STHENL(R(A))=L(A):R(L(A))=R(A):L(A)=0:R(A)=0:Z=Z+1
+ *  260 PRINTC$(Z);:NEXT:PRINTSPC(39-X);&quot;]&quot;;:NEXT
+ *  270 U=INT(RND(1)*X)+1:FORA=XTO1STEP-1
+ *  280 IFR(A)=A-1ORR(A)ANDRND(1)&lt;STHENPRINTC$(1+(A=U));:NEXT:GOTO300
+ *  290 GOSUB20:PRINTC$(3+(A=U));:NEXT
+ *  300 GETA$:IFA$=&quot;&quot;THEN300
+ *  310 IFQ$&lt;&gt;&quot;J&quot;THENRUN140
+ *  320 FORV=1024TO1103+40*H:POKEV,255ANDPEEK(V)-128:POKEV+54272,0:NEXT
+ *  330 V=55296+I:U=55336+40*H:POkE53281,2:GOTO370
+ *  340 IFPEEK(V-54273)AND128THENPOKEV,15AND15-PEEK(V-1):V=V-1:GOTO370
+ *  350 IFNOTPEEK(V-54312)AND2THENPOKEV,15AND15-PEEK(V-40):V=V-40:GOTO340
+ *  360 IFPEEK(V-54272)AND128THENPOKEV,15AND15-PEEK(V+1):V=V+1:GOTO350
+ *  370 IFPEEK(U-54272)AND2THEN340
+ *  380 POKEV,15AND15-PEEK(V+40):V=V+40:IFV&lt;UTHEN360
+ *  390 FORA=0TO49:POKE53281,15AND23-PEEK(53281);FORB=0TO49:NEXT:NEXT
+ *  400 Q$=&quot;&quot;:GOTO300
+ * </pre>
  */
 // TODO convert to progressive row based algorithm
 public class EllerMazeGeneratorC64 implements GridMazeGenerator {
@@ -128,46 +176,3 @@ public class EllerMazeGeneratorC64 implements GridMazeGenerator {
 		return Math.random() < steepness;
 	}
 }
-
-// CLS = reverse heart, clear screen
-// ] = bar on the right
-// J = bar on the right and bottom
-// . = block lower right
-// _ = block on the bottom
-// {Q} = cursor down
-
-// 10 RUN80
-// 20 P=A-1
-// 30 IFR(P)THENP=R(P):GOTO30
-// 40 L(R(A))=P:R(P)=R(A):RETURN
-// 50 P=A
-// 60 IFL(P)THENP=L(P):GOTO60
-// 70 R(L(A-1))=P:L(P)=L(A-1):RETURN
-// 80 POKE53280,0:POKE53281,0:POKE646,8:PRINT"CLS"
-// 140 C$(0)="]":C$(1)="J":C$(2)=".":C$(3)="_":POKE53281,0
-// 150 PRINT:INPUT"{Q}BREEDTE VAN DOOLHOF  (1-39)";X::IFX<1ORX>39THEN150
-// 160 PRINT:INPUT"{Q}HOOGTE VAN DOOLHOF   (2-X)";H:IFH<2THEN150
-// 170 INPUT"STEILHEID ( 0 - .5 - 1 )";S:IFS<0ORS>1THEN170
-// 180 Q$="":IFH<24THENINPUT"OPLOSSEN?(J/N)";Q$:IFQ$<>"J"ANDQ$<>"N"THEN180
-// 190 PRINT"CLS.";:DIML(X),R(X):I=INT(RND(1)*X)+1
-// 200 FORA=1TOX:PRINTC$(3+(A=I));:L(A)=0:R(A)=0:NEXT:PRINTSPC(39-X);"]";
-// 210 FORY=2TOH:FORA=XTO1STEP-1:IFR(A)=A-1ORRND(1)<STHENZ=0:GOTO250
-// 220 IFR(A)THENGOSUB20
-// 230 IFL(A-1)THENGOSUB50
-// 240 R(A)=A-1:L(A-1)=A:Z=2
-// 250 IFL(A)+R(A) AND RND(1)>STHENL(R(A))=L(A):R(L(A))=R(A):L(A)=0:R(A)=0:Z=Z+1
-// 260 PRINTC$(Z);:NEXT:PRINTSPC(39-X);"]";:NEXT
-// 270 U=INT(RND(1)*X)+1:FORA=XTO1STEP-1
-// 280 IFR(A)=A-1ORR(A)ANDRND(1)<STHENPRINTC$(1+(A=U));:NEXT:GOTO300
-// 290 GOSUB20:PRINTC$(3+(A=U));:NEXT
-// 300 GETA$:IFA$=""THEN300
-// 310 IFQ$<>"J"THENRUN140
-// 320 FORV=1024TO1103+40*H:POKEV,255ANDPEEK(V)-128:POKEV+54272,0:NEXT
-// 330 V=55296+I:U=55336+40*H:POkE53281,2:GOTO370
-// 340 IFPEEK(V-54273)AND128THENPOKEV,15AND15-PEEK(V-1):V=V-1:GOTO370
-// 350 IFNOTPEEK(V-54312)AND2THENPOKEV,15AND15-PEEK(V-40):V=V-40:GOTO340
-// 360 IFPEEK(V-54272)AND128THENPOKEV,15AND15-PEEK(V+1):V=V+1:GOTO350
-// 370 IFPEEK(U-54272)AND2THEN340
-// 380 POKEV,15AND15-PEEK(V+40):V=V+40:IFV<UTHEN360
-// 390 FORA=0TO49:POKE53281,15AND23-PEEK(53281);FORB=0TO49:NEXT:NEXT
-// 400 Q$="":GOTO300
